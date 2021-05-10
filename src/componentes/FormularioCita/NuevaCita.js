ddb519855b5780e9eb8af0e8975517a93967c6a7
JeanAlarcon <jeanalexis59@gmail.com>
2021-05-10 15:58:38 -0500

    captura de los imputs y el evento submit

--- src/componentes/FormularioCita/NuevaCita.js
+++ src/componentes/FormularioCita/NuevaCita.js
@@ -1,10 +1,20 @@
 import "./NuevaCita.css"
+import React, { useState } from "react";
 
 function NuevaCita(){
+
+    const [Paciente,setPaciente] = useState("");
+    const [Fecha,setFecha] = useState("");
+    const [Hora,setHora] = useState("");
+
+    const validar = (event) => {
+        event.preventDefault()
+    }
+
     return(
         <div className="principal">
             <div className="wrap">
-                <form className="formulario" action="">
+                <form onSubmit={validar} className="formulario" action="">
                     <h2>Registrar Cita</h2>
                     <input 
                         type="text" 
@@ -14,10 +24,31 @@ function NuevaCita(){
                         minLength="5" 
                         maxLength="40" 
                         pattern="[A-Za-z]"
+                        onChange={(e)=> setPaciente(e.target.value)}
                     />
-                    <input type="date" id="fechaInput" className = "error" placeholder="Fecha"/>
-                    <input type="time" id="horaInput" className = "error" placeholder="Hora"/>
-                    <input type="button" className="boton" id="btn-agregar" value="Agregar Cita"/>
+
+                    <input 
+                        type="date" 
+                        id="fechaInput" 
+                        className= "error" 
+                        placeholder="Fecha"
+                        onChange={(e)=> setFecha(e.target.value)}    
+                    />
+
+                    <input 
+                        type="time" 
+                        id="horaInput" 
+                        className= "error" 
+                        placeholder="Hora"
+                        onChange={(e) => setHora(e.target.value)}
+                    />
+
+                    <input 
+                        type="submit" 
+                        className="boton" 
+                        id="btn-agregar" 
+                        value="Agregar Cita"
+                    />
                 </form>
             </div>
 	    </div>
