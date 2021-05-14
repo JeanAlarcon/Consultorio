edc5d0b4f9905478323faa5e077702eeffca24cf
JeanAlarcon <jeanalexis59@gmail.com>
2021-05-14 13:19:17 -0500

    agrego login y su estilo

--- src/Paginas/Inicio.js
+++ src/Paginas/Inicio.js
@@ -1,9 +1,25 @@
-import "./Pagina.css"
+import "./inicio.css"
 
 function Inicio() {
     return (
-        <div className="container">
-            Inicio
+        <div className="inner">
+            <form>
+
+                <h3>Iniciar secion</h3>
+
+                <div className="form-group">
+                    <label>Email</label>
+                    <input type="email" className="form-control" placeholder="Escribe el email" />
+                </div>
+
+                <div className="form-group">
+                    <label>Contraseña</label>
+                    <input type="password" className="form-control" placeholder="Escribe la contraseña" />
+                </div>
+
+
+                <button type="submit" className="btn btn-dark btn-lg btn-block">Continuar</button>
+            </form>
         </div>
     )
 }
